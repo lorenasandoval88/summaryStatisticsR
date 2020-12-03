813a4c983b47da765dda4b83da3faa23f3de4456
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2020-12-03 09:01:00 -0500

    Update BCAC_summary_and_missingness_statistics_code.R
    matched case "BCAC_Missingness_Statistics.csv"

--- BCAC_summary_and_missingness_statistics_code.R
+++ BCAC_summary_and_missingness_statistics_code.R
@@ -521,7 +521,7 @@ box_ul(109395301106, file="BCAC_Summary_Statistics.csv", pb = options()$boxr.pro
 df_final_miss_stat = MAKE.MISS.STAT(box_id1) # new data - CHANGE BOX ID
 
 #Save the missingness statistics results locally as csv
-write.csv(df_final_miss_stat,"BCAC_missingness_statistics.csv",row.names = FALSE)
+write.csv(df_final_miss_stat,"BCAC_Missingness_Statistics.csv",row.names = FALSE)
 
 #[9a] Upload the file to box using the box folder ID of the BCAC summary and missingness statistics
 box_ul(109395301106, file="BCAC_Missingness_Statistics.csv", pb = options()$boxr.progress,description = NULL) # UPLOAD AS NEW BCAC_missingness_statistics.csv - DON'T CHANGE
